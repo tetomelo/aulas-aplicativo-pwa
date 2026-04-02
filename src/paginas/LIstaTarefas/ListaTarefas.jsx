2111dfadfbfcd4e5d0e4d17e8063ab0b395370fc
Everton Diogo Ferreira Melo <tetoferreira04@gmail.com>
2026-04-01 21:58:20 -0300

    Salvando ListaTarefas

--- src/paginas/LIstaTarefas/ListaTarefas.jsx
+++ src/paginas/LIstaTarefas/ListaTarefas.jsx
@@ -1,11 +1,12 @@
+import "./ListaTarefas.css";
 import { useState } from "react";
 import BotaoCustomizado from "../../componentes/BotaoCustomizado/BotaoCustomizado";
 import CampoCustomizado from "../../componentes/CampoCustomizado/CampoCustomizado";
 import Principal from "../../componentes/Principal/Principal";
+import { FaTrashAlt } from "react-icons/fa";
 
-function ListaTarefas({ }) {
+function ListaTarefas() {
   const [novaTarefa, setNovaTarefa] = useState("");
-
   const [tarefas, setTarefas] = useState([]);
 
   const adicionarTarefa = () => {
@@ -13,31 +14,72 @@ function ListaTarefas({ }) {
       alert("Digite uma tarefa para adicionar!");
       return;
     }
-    console.log(novaTarefa);
 
-    setTarefas([novaTarefa, ...tarefas]);
-};
+    setTarefas([{ concluida: false, descricao: novaTarefa }, ...tarefas]);
+    setNovaTarefa("");
+  };
+
+  const removerTarefa = (indice) => {
+    if (confirm("Tem certeza que deseja remover esta tarefa?")) {
+      tarefas.splice(indice, 1);
+      setTarefas([...tarefas]);
+    }
+  };
+
+  const marcarComoConcluida = (indice) => {
+    tarefas[indice].concluida = !tarefas[indice].concluida;
+    setTarefas([...tarefas]);
+  };
+
 
   return (
     <Principal>
-          <h2>Lista de Tarefas</h2>
+      <h2>Lista de Tarefas({tarefas.length})</h2>
+
+      <div className="lista-tarefas__campo-adicionar">
+        <BotaoCustomizado
+          label="Nova Tarefa"
+        />
 
 
-      <CampoCustomizado
-        label="Nova Tarefa"
-        //propriedades não mapeadas
-        id="nova-tarefa"
-        type="text"
-        placeholder="Precione Enter para adicionar"
-        value={novaTarefa}
-        onChange={(event) => setNovaTarefa(event.target.value)}
-      />
 
+        <CampoCustomizado
+          label="Nova Tarefa"
+          //propriedades não mapeadas
+          id="nova-tarefa"
+          type="text"
+          placeholder="Precione Enter para adicionar"
+          value={novaTarefa}
+          onChange={(event) => setNovaTarefa(event.target.value)}
+          onKeyPress={(event) => {
+            if (event.code === "Enter") {
+              adicionarTarefa();
+            }
+          }}
+        />
 
-      <BotaoCustomizado tipo="secundario" aoClicar={adicionarTarefa}>+</BotaoCustomizado>
+        {/* <BotaoCustomizado tipo="secundario" aoClicar={adicionarTarefa}>
+          +
+        </BotaoCustomizado> */}
+      </div>
 
+      {!tarefas.length && (<span className="lista-tarefas__mensagem-sem-tarefas">Não há tarefas para mostrar.</span>)}
 
-      {JSON.stringify(tarefas, null, 2)}
+      {tarefas.map((item, indice) => {
+        return (
+          <div key={indice} className="lista-tarefas__tarefa">
+            <input
+              type="checkbox"
+              checked={item.concluida}
+              onChange={() => marcarComoConcluida(indice)}
+            />
+            <span style={{ textDecoration: item.concluida ? 'line-through' : 'none' }}>
+              {item.descricao}
+            </span>
+            <FaTrashAlt onClick={() => removerTarefa(indice)} />
+          </div>
+        );
+      })}
     </Principal>
   );
 }
